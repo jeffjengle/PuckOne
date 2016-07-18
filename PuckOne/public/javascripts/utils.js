a06b23df90ccc5a19a81453e608cfc69e4e506f9
Jeff Engle <jeffjengle@hotmail.com>
2016-07-18 13:24:29 -0400

    Added convertTemp

---
+++ PuckOne/public/javascripts/utils.js
@@ -0,0 +1,33 @@
+﻿/* Convert celsius to fahrenheit */
+function convertTemp(origTemp, origType, resultType) {
+    switch (origType.tolowercase()) {
+        case "c":
+            switch (resultType.tolowercase()) {
+                case "f":
+                    return origTemp * 5 / 9 + 32;
+                case "k":
+                    return origTemp + 273.15;
+                default:
+                    return -999999
+            }
+        case "f":
+            switch (resultType.tolowercase()) {
+                case "c":
+                    return (origTemp - 32) * 5 / 9;
+                case "k":
+                    return (origTemp + 459.67) * 5 / 9 ;
+                default:
+                    return -999999
+            }
+        case "k":
+            switch (resultType.tolowercase()) {
+                case "c":
+                    return origTemp - 273.15;
+                case "f":
+                    return origTemp * 9 / 5 - 459.67;
+                default:
+                    return -999999
+            }
+    }
+}
+module.exports.convertTemp = convertTemp;
--- PuckOne/routes/users.js
+++ PuckOne/routes/users.js
@@ -1,19 +1,19 @@
 ﻿var express = require('express');
 var router = express.Router();
 var osInfo = require('os')
-var commandToExecute = 'dir'
 
 /* GET users listing. */
 router.get('/', function (req, res) {
 
     const exec = require('child_process').exec;
-    if (osInfo.platform() === 'linux') { commandToExecute = 'vcgencmd measure_temp' }
+    if (osInfo.platform() === 'linux') { var commandToExecute = 'vcgencmd measure_temp' }
     exec(commandToExecute, function(err, stdout, stderr) {
         if (err) {
             console.error(err);
             return;
         }
-        var cpuTempF = stdout.split("=").pop();
+        var cpuTemp = stdout.split("=").pop();
+        var cpuTempF = convertTemp(cpuTemp, "C", "F");
         console.log(cpuTempF);
         res.send(cpuTempF);
     });
