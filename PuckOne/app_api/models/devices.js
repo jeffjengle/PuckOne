321d337d3f0902e8e9e3c83392c0d9da716423e5
Jeff Engle <jeffjengle@hotmail.com>
2016-08-31 17:22:17 -0400

    Starting to add mangodb

---
+++ PuckOne/app_api/models/devices.js
@@ -0,0 +1,10 @@
+﻿var mongoose = require('mongoose');
+
+var deviceSchema = new mongoose.Schema({
+    temperature: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('Device', deviceSchema);
